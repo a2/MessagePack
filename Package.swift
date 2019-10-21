a3055d1f39581ec259d899b4c2d9e5c1fd4e1be8
Jason Boyle <jason.boyle@workday.com>
2019-10-21 16:00:27 -0700

    Update package.swift

--- Package.swift
+++ Package.swift
@@ -1,4 +1,22 @@
 // swift-tools-version:5.1
 import PackageDescription
 
-let package = Package(name: "MessagePack")
+let package = Package(
+  name: "MessagePack",
+  platforms: [
+    .iOS(.v11)
+  ],
+  products: [
+    .library(
+      name: "MessagePack",
+      targets: ["MessagePack"]
+    ),
+  ],
+  dependencies: [],
+  targets: [
+    .target(
+      name: "MessagePack",
+      dependencies: []
+    )
+  ]
+)
